a7879b3b741fff1684d4fa8b2b8457a568c5c24d
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2018-10-01 22:52:21 +0100

    dont inline font files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   indexPath: '../index.html',
-  assetsDir: '../dist'
+  assetsDir: '../dist',
+
+  chainWebpack: config => {
+    config.module
+      .rule('fonts')
+        .use('url-loader')
+          .loader('url-loader')
+          .tap(options => Object.assign(options, { limit: 1 }))
+  }
 }
